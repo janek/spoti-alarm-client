fb7658bfc5cc5b7ec3bf486d5f99ecc7e33ba238
Janek Szynal <jan.szynal@gmail.com>
2018-12-23 02:35:11 +0100

    Initial Commit

---
+++ raspi-alarm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  raspi-alarm
+//
+//  Created by Janek Szynal on 23.12.2018.
+//  Copyright © 2018 Janek Szynal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
