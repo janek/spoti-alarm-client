d37246670f854ebd93d3f23c53a453098f1b4afe
Janek Szynal <jan.szynal@gmail.com>
2020-03-06 23:07:51 +0100

    Prepare a basic version of the project for adding UIKit

--- raspi-alarm/ViewController.swift
+++ raspi-alarm/ViewController.swift
@@ -10,34 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {    
 
-    @IBOutlet weak var picker: UIDatePicker!
-    @IBOutlet weak var feedbackLabel: UILabel!
-    @IBOutlet weak var setAlarmButton: UIButton!
-    @IBOutlet weak var musicSourceSegmentedControl: UISegmentedControl!
-    @IBOutlet weak var roomSegmentedControl: UISegmentedControl!
+//    @IBOutlet weak var picker: UIDatePicker!
+
+//    @IBOutlet weak var musicSourceSegmentedControl: UISegmentedControl!
+//    @IBOutlet weak var roomSegmentedControl: UISegmentedControl!
+
+    var setAlarmButton: UIButton!
+    var feedbackLabel: UILabel!
+
+    let deviceWidth = UIScreen.main.bounds.size.width
+    let deviceHeight = UIScreen.main.bounds.size.height
 
     var mode: MusicMode = .Spotify
-    var availableModes = [MusicMode.Spotify, .Luz] // TODO: change to allCases after updating to newer swift
-
+    var availableModes = [MusicMode.Spotify, .Luz] // TODO: change to allCases in Swift 5
     var room: Room = .Janek
-    var availableRooms = [Room.Janek, .Mila]
 
     let networker = Networker.shared
 
     override func viewDidLoad() {
-        musicSourceSegmentedControl.addTarget(self, action: #selector(musicSourceValueChanged(segmentedControl:)), for: .valueChanged)
-        roomSegmentedControl.addTarget(self, action: #selector(roomValueChanged(segmentedControl:)), for: .valueChanged)
-        picker.setValue(Color.spotifyGreen.value, forKeyPath: "textColor") //TODO: set initial state, not initial colors
+
+//        musicSourceSegmentedControl.addTarget(self, action: #selector(musicSourceValueChanged(segmentedControl:)), for: .valueChanged)
+//        roomSegmentedControl.addTarget(self, action: #selector(roomValueChanged(segmentedControl:)), for: .valueChanged)
+//        picker.setValue(Color.spotifyGreen.value, forKeyPath: "textColor") //TODO: set initial state, not initial colors
+
+        setAlarmButton = UIButton(frame: CGRect(x: 100, y: 300, width: 50, height: 50)) // TODO: actual positioning
+        setAlarmButton.setTitle("SET", for: .normal)
+        setAlarmButton.backgroundColor = Color.spotifyGreen.value
+        setAlarmButton.layer.cornerRadius = 10
+
+
+        let labelHeight: CGFloat = 40
+        feedbackLabel = UILabel(frame: CGRect(x: 0, y: deviceHeight - labelHeight, width: deviceWidth, height: labelHeight))
+
+        view.addSubview(setAlarmButton)
+        view.addSubview(feedbackLabel)
     }
 
-    @IBAction func setAlarmClock() {
-        let formatter = DateFormatter()
-        formatter.dateFormat = "mm"
-        let minutes = formatter.string(from: picker.date)
-        formatter.dateFormat = "H"
-        let hours = formatter.string(from: picker.date)
+    func setAlarmClock() {
+//        let formatter = DateFormatter()
+//        formatter.dateFormat = "mm"
+//        let minutes = formatter.string(from: picker.date)
+//        formatter.dateFormat = "H"
+//        let hours = formatter.string(from: picker.date)
         
-        networker.sendScheduleRequestToServer(minutes: minutes, hours: hours, mode: self.mode, room: self.room, completion: { response in
+        networker.sendScheduleRequestToServer(minutes: "30", hours: "9", mode: self.mode, room: self.room, completion: { response in
             print(response)
             self.feedbackLabel.text = response + "\n" + self.feedbackLabel.text!
             self.feedbackLabel.sizeToFit()
@@ -49,14 +65,14 @@ class ViewController: UIViewController {
 
         switch chosenSourceString {
         case MusicMode.Spotify.rawValue:
-            picker.setValue(Color.spotifyGreen.value, forKeyPath: "textColor")
-            segmentedControl.tintColor = Color.spotifyGreen.value
+//            picker.setValue(Color.spotifyGreen.value, forKeyPath: "textColor")
+//            segmentedControl.tintColor = Color.spotifyGreen.value
             setAlarmButton.backgroundColor = Color.spotifyGreen.value
             feedbackLabel.textColor = Color.spotifyGreen.value
             mode = .Spotify
         case MusicMode.Luz.rawValue:
-            picker.setValue(Color.luzOrange.value, forKeyPath: "textColor")
-            segmentedControl.tintColor = Color.luzOrange.value
+//            picker.setValue(Color.luzOrange.value, forKeyPath: "textColor")
+//            segmentedControl.tintColor = Color.luzOrange.value
             setAlarmButton.backgroundColor = Color.luzOrange.value
             feedbackLabel.textColor = Color.luzOrange.value
             mode = .Luz
---
+++ raspi-alarm/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  raspi-alarm
+//
+//  Created by Janek Szynal on 06.03.20.
+//  Copyright © 2020 Janek Szynal. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+_ = UIApplicationMain(CommandLine.argc, CommandLine.unsafeArgv, nil, NSStringFromClass(AppDelegate.self))
